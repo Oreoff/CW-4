0a5fe8c7a20bae4f3e0cc4dd548d79877e9f38ca
Oreoff <maximmelnik37@gmail.com>
2024-03-02 12:20:24 +0200

    added navHost and startScreen

--- app/src/main/java/ua/edu/lntu/ipz_32_cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_32_cw_4/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.ipz_32_cw_4.ui.theme.IPZ_32_cw_4Theme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ua.edu.lntu.ipz_32_cw_4.ui.StartScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +29,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
+sealed class Screen(val route: String) {
+    object StartScreen : Screen("StartScreen")
+    object SecondScreen : Screen("SecondScreen")
+}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.StartScreen.route,
+        modifier = Modifier
+    ) {
+composable(Screen.StartScreen.route)
+{
+StartScreen(navController = navController)
+}
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IPZ_32_cw_4Theme {
-        Greeting("Android")
+        Greeting()
     }
 }
