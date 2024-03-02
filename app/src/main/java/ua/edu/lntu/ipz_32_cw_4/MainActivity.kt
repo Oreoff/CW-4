7831c6abe99e183652456c81acd9a2926dd110cb
Oreoff <maximmelnik37@gmail.com>
2024-03-02 13:04:38 +0200

    Added AppBar

--- app/src/main/java/ua/edu/lntu/ipz_32_cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_32_cw_4/MainActivity.kt
@@ -1,11 +1,15 @@
 package ua.edu.lntu.ipz_32_cw_4
-
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,47 +22,61 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ua.edu.lntu.ipz_32_cw_4.ui.StartScreen
+import androidx.compose.material3.TopAppBar
 
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             IPZ_32_cw_4Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Greeting()
                 }
             }
         }
     }
 }
 sealed class Screen(val route: String) {
-    object StartScreen : Screen("StartScreen")
-    object SecondScreen : Screen("SecondScreen")
+    data object StartScreen : Screen("StartScreen")
+    data object SecondScreen : Screen("SecondScreen")
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = Screen.StartScreen.route,
-        modifier = Modifier
-    ) {
-composable(Screen.StartScreen.route)
-{
-StartScreen(navController = navController)
-}
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Navigation Example") },
+                modifier = modifier,
+            )
+        }
+    ) {innerPadding -> Modifier.padding(25.dp)
+        NavHost(
+            navController = navController,
+            startDestination = Screen.StartScreen.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = Screen.StartScreen.route)
+            {
+                StartScreen(navController = navController, modifier = Modifier)
+            }
+        }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_32_cw_4Theme {
-        Greeting()
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        IPZ_32_cw_4Theme {
+            Greeting()
+        }
     }
-}
---
+++ app/src/main/java/ua/edu/lntu/ipz_32_cw_4/ui/StartScreen.kt
@@ -0,0 +1,35 @@
+package ua.edu.lntu.ipz_32_cw_4.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+import androidx.compose.ui.tooling.preview.Preview
+import ua.edu.lntu.ipz_32_cw_4.ui.theme.IPZ_32_cw_4Theme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun StartScreen(modifier: Modifier = Modifier, navController: NavController)
+{
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "Start screen",
+            fontSize = 20.sp,
+        )
+    }
+}
